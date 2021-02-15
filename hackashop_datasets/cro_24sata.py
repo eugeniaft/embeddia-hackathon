9bb0bd9a571ec9da6b3b1f23e0fb988336555465
dkorenci <damir.korencic@gmail.com>
2021-02-15 19:27:07 +0100

    creating and validating train-dev-test split for the croatian dataset

--- hackashop_datasets/cro_24sata.py
+++ hackashop_datasets/cro_24sata.py
@@ -33,7 +33,7 @@ def label_distribution(dset):
     for label in dset['infringed_on_rule']:
         if isnan(label): nonblck += 1 # not blocked
         else:
-            if not label in lmap: lmap[label] = 0.0
+            if not label in lmap: lmap[label] = 1.0
             else: lmap[label] += 1
     blck = N - nonblck
     print(f"all: {int(N)}")
@@ -57,13 +57,22 @@ def cro24sata_filterbyyear(year, fname='STY_24sata_comments_hr_001'):
     save_dir = Path(CRO_24SATA_DATASET); save_file = save_dir / fname
     dset.to_pickle(save_file, compression=None)
 
-def cro24sata_load_byyear(year, fname='STY_24sata_comments_hr_001'):
-    fname = fname + f"_year{year}.pickle"
+def cro24sata_load_byyear(year, fname='STY_24sata_comments_hr_001', label=''):
+    fname = fname + f"_year{year}{label}.pickle"
     save_dir = Path(CRO_24SATA_DATASET); save_file = save_dir / fname
     dset = pd.read_pickle(save_file, compression=None)
     return dset
 
 cro24_labelset_offensive = ([2.0, 3.0, 8.0], [1.0])
+
+def cro24data_label_sample(label, sample_size=200):
+    dset = cro24sata_load_byyear(2019)
+    label_column = dset['infringed_on_rule']
+    text_column = dset['content']
+    texts = [text_column[i] for i in dset.index if label_column[i] == label]
+    shuffle(texts)
+    for t in texts[:sample_size]:
+        print(t.replace('\n', ' '))
 
 def cro24sata_unbalanced_offensive():
     dset = cro24sata_load_byyear(2019)
@@ -82,9 +91,66 @@ def cro24sata_unbalanced_offensive():
         texts.append(text); labels.append(addlabel)
     return texts, labels
 
+def cro24sata_filter_categories(year, categories, label):
+    dset = cro24sata_load_byyear(2019)
+    print(dset.shape)
+    indices = []; labels = dset['infringed_on_rule']
+    for i in dset.index:
+        l = labels[i]
+        if isnan(l) or l not in categories: indices.append(i)
+    #print(indices)
+    dset = dset.loc[indices]
+    print_dataset(dset)
+    fname = f"STY_24sata_comments_hr_001_year{year}{label}.pickle"
+    save_dir = Path(CRO_24SATA_DATASET); save_file = save_dir / fname
+    dset.to_pickle(save_file, compression=None)
+
+def cro24sata_balance_dataset(year, label, train_sz, dev_sz, test_sz):
+    from sklearn.model_selection import train_test_split
+    dset = cro24sata_load_byyear(year, label=label)
+    label_column = dset['infringed_on_rule']
+    indices = []; labels = []
+    for i in dset.index:
+        indices.append(i)
+        l = label_column[i]
+        if isnan(l): labels.append(0);
+        else: labels.append(int(l))
+    train_ix, td_ix, train_lab, td_lab = train_test_split(indices, labels, train_size=train_sz,
+                                           test_size=dev_sz+test_sz, stratify=labels)
+    test_ix, dev_ix, test_lab, dev_lab = train_test_split(td_ix, td_lab, train_size=test_sz,
+                                                              test_size=dev_sz, stratify=td_lab)
+    parts = {
+        'train' : dset.loc[train_ix],
+        'test' : dset.loc[test_ix],
+        'dev' : dset.loc[dev_ix]
+    }
+    for part in ['train', 'test', 'dev']:
+        fname = f"STY_24sata_comments_hr_001_year{year}{label}_{part}.pickle"
+        save_dir = Path(CRO_24SATA_DATASET); save_file = save_dir / fname
+        parts[part].to_pickle(save_file, compression=None)
+
+def cro24_validate_split():
+    train = cro24sata_load_byyear(2019, label='_nosmallcat_train')
+    dev = cro24sata_load_byyear(2019, label='_nosmallcat_dev')
+    test = cro24sata_load_byyear(2019, label='_nosmallcat_test')
+    print('--------- train'); label_distribution(train)
+    print('--------- dev'); label_distribution(dev)
+    print('--------- test'); label_distribution(test)
+    ixtr = set(train.index)
+    ixts = set(test.index)
+    ixdv = set(dev.index)
+    assert(len(ixtr.intersection(ixts))==0)
+    assert(len(ixtr.intersection(ixdv)) == 0)
+    assert(len(ixts.intersection(ixdv)) == 0)
+
 if __name__ == '__main__':
     #cro24sata_explore()
     #cro24sata_filterbyyear(2019)
-    #print_dataset(cro24sata_load_byyear(2019))
+    #print_dataset(cro24sata_load_byyear(2019, label='_nosmallcat'))
     #label_distribution(cro24sata_load_byyear(2019))
-    cro24sata_unbalanced_offensive()
+    #cro24sata_unbalanced_offensive()
+    #cro24data_label_sample(4.0)
+    #cro24sata_filter_categories(2019, [2.0, 4.0, 7.0], '_nosmallcat')
+    #cro24sata_balance_dataset(2019, '_nosmallcat', 40000, 10000, 10000)
+    cro24_validate_split()
+
