be1bed1872e9b36f461211b0e02c09500d9734f5
eugeniaft <euge.ft@gmail.com>
2021-02-15 10:45:48 -0500

    adding data split function

--- hackashop_datasets/load_data.py
+++ hackashop_datasets/load_data.py
@@ -13,3 +13,27 @@ def load_toxic_en_data():
 	d3 = ynacc.load_ynacc_data()
 
 	return tuple(x + y + z for x, y, z in zip(d1, d2, d3))
+
+
+def train_dev_test(data, labels, random_seed):
+
+	# split data into train and test
+    train, test, train_labels, test_labels = train_test_split(data, 
+    	labels, test_size=0.1, stratify=labels, random_state=random_seed)
+
+    # split train into train and dev
+    train, dev, train_labels, dev_labels = train_test_split(train, 
+    	train_labels, test_size=0.1/0.9, stratify=train_labels, 
+    	random_state=random_seed)
+
+    split_data = {'train': (train, train_labels),
+                  'dev': (dev, dev_labels),
+                  'test': (test, test_labels)
+                  }
+                  
+    return split_data
+
+
+
+
+
